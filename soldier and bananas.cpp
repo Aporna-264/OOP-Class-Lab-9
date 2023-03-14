ff219ce96caa8cc23874d0c1f5186c02913fd44e
Aporna Bhoumick <122268415+Aporna-264@users.noreply.github.com>
2023-03-14 11:23:11 +0600

    Add files via upload

---
+++ soldier and bananas.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i, n, k, w;
+	long long sum=0;
+
+	cin>> k>>n>>w;
+	for(i=1; i<=w; i++){
+		sum = sum+(i*k);
+	}
+	if(sum<= n){
+		cout <<0;
+	}
+	else
+	    cout << sum - n;
+
+	return 0;
+}
+
